9b50bc1c17615836a76ce07d0a1d8cfe92c88bd2
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-10 14:51:20 +0100

    export gallery token

--- libs/elbe/gallery/src/index.ts
+++ libs/elbe/gallery/src/index.ts
@@ -3,5 +3,6 @@ import { ElbGalleryItem } from './lib/elb-gallery-item';
 
 export * from './lib/elb-gallery';
 export * from './lib/elb-gallery-item';
+export * from './lib/elb-gallery.token';
 
 export const ElbGalleryImports = [ElbGallery, ElbGalleryItem] as const;
