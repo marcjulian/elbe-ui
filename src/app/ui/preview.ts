fcce16f07190bd26e4a30c48b73ac617e5cbaa5a
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-13 14:33:30 +0100

    update preview selector and classes

--- src/app/ui/preview.ts
+++ src/app/ui/preview.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { computed, Directive, input } from '@angular/core';
+import { hlm } from '@spartan-ng/helm/utils';
+import { ClassValue } from 'clsx';
 
 @Directive({
-  selector: '[elbPreview]',
+  selector: '[elbPreview],elb-preview',
   host: {
-    class:
-      'border-border flex items-center justify-center rounded-md border min-h-60 p-6 md:p-10 mt-4',
+    'data-slot': 'preview',
+    '[class]': '_computedClass()',
   },
 })
-export class Preview {}
+export class Preview {
+  public readonly _userClass = input<ClassValue>('', {
+    alias: 'class',
+  });
+  protected readonly _computedClass = computed(() =>
+    hlm(
+      'border-border relative mt-4 flex min-h-60 items-center justify-center rounded-md border p-6 md:p-10',
+      this._userClass(),
+    ),
+  );
+}
