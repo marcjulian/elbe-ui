aa0b7667702b721cae48ce98f1a9e03040e32d2e
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-15 20:05:17 +0100

    update image alt description

--- src/app/pages/components/gallery/images.ts
+++ src/app/pages/components/gallery/images.ts
@@ -9,7 +9,6 @@ export const images: {
 }[] = [
   {
     src: 'https://images.unsplash.com/photo-1598926737692-2dc95228f1ed?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    // white-kayak-on-lake-near-green-trees-during-daytime
     alt: 'White kayak on lake near green trees during daytime',
     width: 2532,
     height: 1424,
@@ -20,7 +19,8 @@ export const images: {
   },
   {
     src: 'https://images.unsplash.com/photo-1515409116264-67afef5e27a7?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Green vegetable on white ceramic bowl',
+    // person-riding-on-boat-paddle-during-sunset
+    alt: 'Person riding on boat paddle during sunset',
     width: 1587,
     height: 2380,
     previewUrl:
@@ -30,7 +30,7 @@ export const images: {
   },
   {
     src: 'https://images.unsplash.com/photo-1681308065147-4c0a0777a969?q=80&w=2675&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Noodles with sliced lemon on black ceramic bowl',
+    alt: 'A row of houses next to Elbe River',
     width: 2675,
     height: 1773,
     previewUrl:
