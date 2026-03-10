829aeb848908c275d1b4840360b0f7bbf57f12b9
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-03-10 09:13:44 +0100

    disable zoom in/out button based on max/min zoom level

--- libs/elbe/map/src/lib/elb-navigation-control.ts
+++ libs/elbe/map/src/lib/elb-navigation-control.ts
@@ -1,5 +1,14 @@
 import { BooleanInput } from '@angular/cdk/coercion';
-import { booleanAttribute, ChangeDetectionStrategy, Component, inject, input } from '@angular/core';
+import {
+  afterNextRender,
+  booleanAttribute,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
 import { ControlComponent, MapService } from '@maplibre/ngx-maplibre-gl';
 import { provideIcons } from '@ng-icons/core';
 import { lucideMinus, lucidePlus } from '@ng-icons/lucide';
@@ -22,7 +31,7 @@ import type { ControlPosition } from 'maplibre-gl';
           size="icon-sm"
           type="button"
           class="dark:bg-background dark:hover:bg-background/80"
-          [disabled]="disabled()"
+          [disabled]="_disabledZoomIn()"
           (click)="_zoomIn()"
         >
           <ng-icon name="lucidePlus" />
@@ -33,7 +42,7 @@ import type { ControlPosition } from 'maplibre-gl';
           size="icon-sm"
           type="button"
           class="dark:bg-background dark:hover:bg-background/80"
-          [disabled]="disabled()"
+          [disabled]="_disabledZoomOut()"
           (click)="_zoomOut()"
         >
           <ng-icon name="lucideMinus" />
@@ -45,9 +54,34 @@ import type { ControlPosition } from 'maplibre-gl';
 export class ElbNavigationControl {
   private mapService = inject(MapService);
 
+  private readonly _zoom = signal(0);
+  private readonly _maxZoom = signal(0);
+  private readonly _minZoom = signal(0);
+
   public readonly position = input<ControlPosition>();
 
   public readonly disabled = input<boolean, BooleanInput>(false, { transform: booleanAttribute });
+
+  protected readonly _disabledZoomIn = computed(
+    () => this.disabled() || this._zoom() >= this._maxZoom(),
+  );
+  protected readonly _disabledZoomOut = computed(
+    () => this.disabled() || this._zoom() <= this._minZoom(),
+  );
+
+  constructor() {
+    afterNextRender(() => {
+      this.updateZoomState();
+      this.mapService.mapInstance.on('zoom', () => this.updateZoomState());
+    });
+  }
+
+  private updateZoomState() {
+    const map = this.mapService.mapInstance;
+    this._zoom.set(map.getZoom());
+    this._maxZoom.set(map.getMaxZoom());
+    this._minZoom.set(map.getMinZoom());
+  }
 
   protected _zoomIn() {
     this.mapService.mapInstance.zoomIn();
