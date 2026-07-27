93851e601dddfbe55d7fc9a503795cc377641910
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2026-07-27 15:56:28 +0200

    update maplibre v6

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { setWorkerUrl } from 'maplibre-gl';
 import { App } from './app/app';
 import { appConfig } from './app/app.config';
+
+setWorkerUrl(new URL('maplibre-gl-worker.mjs', document.baseURI).href);
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
